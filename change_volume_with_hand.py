e1b0f2d3d766c34d696a2a1882eab85abefcf661
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2024-01-22 13:53:19 +0530

    feature: Improved user response
    Mute, decrease and increase working awesome.

--- change_volume_with_hand.py
+++ change_volume_with_hand.py
@@ -7,6 +7,8 @@ import keyboard
 # ? Multithreading
 
 
+# TODO: Add code documentations by your standards
+# * Class name should be set to something beautiful
 class GestureControl:
     def __init__(self) -> None:
         pass
@@ -76,13 +78,21 @@ drawing_utils = mp.solutions.drawing_utils  # draw points in hand
 
 mute_state = False  # Variable to track mute state
 
-while True:
+while (
+    True
+):  # ! True are a bad practice, and should be not used in critical programs without further safety meaures.
     _, image = webcam.read()
     image = cv2.flip(image, 1)  # 0 = x axis and 1 = y axis
+    # ? Why flipping was used? It flips the image?
+    # ? What useful purpose does it serves?
     frame_height, frame_width, _ = image.shape
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    rgb_image = cv2.cvtColor(
+        image, cv2.COLOR_BGR2RGB
+    )  # ? How this works? Will it be efficient if b/w is used or RGB is just for the user?
     output = my_hands.process(rgb_image)
     hands = output.multi_hand_landmarks
+
     if hands:
         for hand in hands:
             drawing_utils.draw_landmarks(image, hand)
@@ -90,13 +100,15 @@ while True:
             for id, landmarks in enumerate(landmarks):
                 x = int(landmarks.x * frame_width)
                 y = int(landmarks.y * frame_height)
+
+                # ** Drawing circles are good
                 if id == 8:  # forefinger id is 8
                     cv2.circle(
                         img=image,
                         center=(x, y),
-                        radius=8,
+                        radius=16,
                         color=(0, 255, 255),
-                        thickness=3,
+                        thickness=2,
                     )
                     x1 = x
                     y1 = y
@@ -104,15 +116,16 @@ while True:
                     cv2.circle(
                         img=image,
                         center=(x, y),
-                        radius=8,
+                        radius=16,
                         color=(0, 0, 255),
-                        thickness=3,
+                        thickness=2,
                     )
                     x2 = x
                     y2 = y
 
+        # ** cut here, distance formula
         dist = ((((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** (0.5)) // 4
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
         # if dist > 20:
         #     pyautogui.press("volumeup")
         # # elif dist < 8:
@@ -120,10 +133,15 @@ while True:
         # elif dist < 20:
         #     pyautogui.press("mute")
 
-        if dist > 22:
+        # ** cut here
+        # TODO: mute_state can be further thought like mute should stay mute when increasing or decreasing volume or not
+        if dist > 20:
             keyboard.press_and_release("volume up")
             mute_state = False  # Unmute when adjusting volume
-        elif dist < 10 and not mute_state:
+        elif dist < 14 and dist > 10:
+            keyboard.press_and_release("volume down")
+            mute_state = False
+        elif dist < 2.2 and not mute_state:
             keyboard.press_and_release("volume mute")
             mute_state = True
             # ? Learn more about CONSTANTS, what are those and how to go on with them.
@@ -132,8 +150,10 @@ while True:
         # else:
         #     keyboard.press_and_release("volume down")
 
+    # ** cut here
     cv2.imshow("Hand volume control using Python", image)
     key = cv2.waitKey(10)
+    # ? Why cv2.waitkey is used here?
     if key == 27:  # 27 is ASCII of ESC key
         break
 
